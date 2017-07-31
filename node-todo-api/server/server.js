bd9a565cb59577ad032268edeb8beffa1d8f71ab
Huiqiong Gu <hgu@walmartlabs.com>
2017-07-30 17:11:53 -0700

    adding get todo route handler

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const Todo = require("./models/todos");
@@ -27,7 +28,23 @@ app.get("/todos", (req, res) => {
   }, err => {
     res.status(400).send(err);
   });
-})
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send({});
+  } else {
+    Todo.findById(id).then(todo => {
+      if (!todo) {
+        return res.status(404).send({});
+      }
+      res.status(200).send({todo});
+    }).catch(err => {
+      res.status(400).send({});
+    });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
